966ba537aa1a3364f1485918fa559f07db24e912
0xEVG <evgenikrolikov@gmail.com>
2022-07-24 08:37:43 +0700

    done?

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Loading } from './components/Loading/Loading';
+import { Table } from './components/Table/Table';
+import { useFetch } from './hooks/useFetch';
+import { filterTable } from './utils/filter';
+import { sortTable } from './utils/sortTable';
+
 
 function App() {
+  const host = "http://localhost:8081/api/table";
+  const [table] = useFetch(host);
+
+  const filter = filterTable;
+
+  const sortFunc = sortTable;
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      {table?
+      <Table table={table.table} 
+              columns={table.columns} 
+              filterFunc={filter} 
+              sortFunc={sortFunc}
+              sortedColumnNumber={1}
+                        />
+        :
+        <Loading/>
+        }
     </div>
   );
 }
---
+++ src/hooks/useForm.ts
@@ -0,0 +1,18 @@
+import { ChangeEvent, useState } from "react";
+
+
+export function useForm<T>(inputs: T, onChangeFunc?:(event: ChangeEvent<any>, state: T)=>T): [T, (event: ChangeEvent<any>) => void]{
+    
+    const [inputsState, setInputsState] = useState<T>(inputs)
+    function onChange(event: ChangeEvent<any>) {
+        
+        setInputsState((oldState)=>{
+            const newState: T = onChangeFunc? onChangeFunc(event, oldState):{...oldState}
+            newState[event.target.name as keyof T] = event.target.value;
+            console.log(newState);
+            return newState;
+        })    
+    }
+
+    return [inputsState, onChange];
+}
